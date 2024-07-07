9b54f63f3a2da16e57cab0891a3bf1fb8d68080e
Nattapong <64143172@g.cmru.ac.th>
2024-07-07 14:20:38 +0700

    1212

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -12,34 +12,56 @@ class CalculationScreen extends StatefulWidget {
 }
 
 class _CalculationScreenState extends State<CalculationScreen> {
+  double amount = 0;
+  double interest = 0;
+  double result = 0;
+  void calculate() {
+    setState(() {
+      result = amount + (amount * interest) / 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator'),
+        centerTitle: true,
+        title: const Text('Calculator'),
       ),
       body: Container(
           child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            Text("จำนวนเงินที่ต้องจ่าย"),
+            Text("จำนวนเงินที่ต้องจ่าย ${result.toStringAsFixed(2)} บาท"),
             Form(
                 child: Column(
               children: [
                 TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(labelText: "กรอกวงเงิน"),
+                  onChanged: (value) {
+                    setState(() {
+                      amount = double.parse(value);
+                    });
+                  },
                 ),
                 TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(labelText: "อัตราดอกเบี้ย"),
+                  onChanged: (value) {
+                    setState(() {
+                      interest = double.parse(value);
+                    });
+                  },
                 ),
                 SizedBox(
                   height: 20,
                 ),
                 ElevatedButton.icon(
-                    onPressed: () {},
+                    onPressed: () {
+                      calculate();
+                    },
                     icon: Icon(Icons.calculate),
                     label: Text('คำนวน')),
               ],
